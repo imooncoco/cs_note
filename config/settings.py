bcc4e112597ea9d0c82138156e037e68f31854b3
imooncoco <imooncoco@gmail.com>
2021-05-03 14:33:22 +0900

    setting

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--9nc-^nh4kv2i(
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['aprkmsboard.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
